33d56375db5b315ddefef33ad9fce43ec6e2e107
icorreavilla1 <167145467+icorreavilla1@users.noreply.github.com>
2024-10-26 18:50:24 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -1,149 +1,87 @@
-import os
-import streamlit as st
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-from PIL import Image
-import time
-import glob
 import cv2
+import yolov5
+import streamlit as st
 import numpy as np
-import pytesseract
-from gtts import gTTS
-from googletrans import Translator
+import pandas as pd
+#from ultralytics import YOLO
 
-st.title("Traductor de imágenes")
-st.subheader("Reconocimiento óptico de imágenes y traducción de texto")
+#import sys
+#sys.path.append('./ultralytics/yolo')
 
-translator = Translator()  # Inicializar el traductor
+#from utils.checks import check_requirements
 
-# Mostrar una imagen de ejemplo en la interfaz
-image = Image.open('traductorimg.png')
-st.image(image, width=300)
+
+# load pretrained model
+model = yolov5.load('yolov5s.pt')
+#model = yolov5.load('yolov5nu.pt')
+
+# set model parameters
+model.conf = 0.25  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.max_det = 1000  # maximum number of detections per image
+
+# take a picture with the camera
+st.title("Detección de Objetos en Imágenes")
 
 with st.sidebar:
-    st.subheader("Traductor.")
-    st.write("Presiona el botón, cuando escuches la señal "
-             "habla lo que quieres traducir, luego selecciona"
-             " la configuración de lenguaje que necesites.")
+            st.subheader('Parámetros de Configuración')
+            model.iou= st.slider('Seleccione el IoU',0.0, 1.0)
+            st.write('IOU:', model.iou)
 
-st.write("Toca el Botón y habla lo que quieres traducir")
+with st.sidebar:
+            model.conf = st.slider('Seleccione el Confidence',0.0, 1.0)
+            st.write('Conf:', model.conf)
 
-stt_button = Button(label=" Escuchar  🎤", width=300, height=50)
 
-stt_button.js_on_event("button_click", CustomJS(code="""
-    var recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.interimResults = true;
+picture = st.camera_input("Capturar foto",label_visibility='visible' )
 
-    recognition.onresult = function (e) {
-        var value = "";
-        for (var i = e.resultIndex; i < e.results.length; ++i) {
-            if (e.results[i].isFinal) {
-                value += e.results[i][0].transcript;
-            }
-        }
-        if (value != "") {
-            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-        }
-    }
-    recognition.start();
-    """))
+if picture:
+    #st.image(picture)
 
-result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0
-)
-
-img_file_buffer = st.camera_input("Toma una Foto")
-
-if img_file_buffer is not None:
-    # Leer la imagen desde el buffer
-    bytes_data = img_file_buffer.getvalue()
+    bytes_data = picture.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+  
+    # perform inference
+    results = model(cv2_img)
 
-    # Convertir la imagen a RGB y extraer el texto
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text = pytesseract.image_to_string(img_rgb)
-    st.write("Texto detectado:")
-    st.write(text)
+    # parse results
+    predictions = results.pred[0]
+    boxes = predictions[:, :4] 
+    scores = predictions[:, 4]
+    categories = predictions[:, 5]
 
-    # Selección de idioma de entrada
-    in_lang = st.selectbox(
-        "Selecciona el lenguaje de Entrada",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés")
-    )
-    if in_lang == "Inglés":
-        input_language = "en"
-    elif in_lang == "Español":
-        input_language = "es"
-    elif in_lang == "Bengali":
-        input_language = "bn"
-    elif in_lang == "Coreano":
-        input_language = "ko"
-    elif in_lang == "Mandarín":
-        input_language = "zh-cn"
-    elif in_lang == "Japonés":
-        input_language = "ja"
+    col1, col2 = st.columns(2)
 
-    # Selección de idioma de salida
-    out_lang = st.selectbox(
-        "Selecciona el lenguaje de salida",
-        ("Inglés", "Español", "Bengali", "Coreano", "Mandarín", "Japonés")
-    )
-    if out_lang == "Inglés":
-        output_language = "en"
-    elif out_lang == "Español":
-        output_language = "es"
-    elif out_lang == "Bengali":
-        output_language = "bn"
-    elif out_lang == "Coreano":
-        output_language = "ko"
-    elif out_lang == "Mandarín":
-        output_language = "zh-cn"
-    elif out_lang == "Japonés":
-        output_language = "ja"
+    with col1:
+        # show detection bounding boxes on image
+        results.render()
+        # show image with detections 
+        st.image(cv2_img, channels = 'BGR')
 
-    def text_to_speech(input_language, output_language, text, tld='com'):
-        translation = translator.translate(text, src=input_language, dest=output_language)
-        trans_text = translation.text
-        tts = gTTS(trans_text, lang=output_language, tld=tld, slow=False)
-        my_file_name = text[0:20].replace(" ", "_")
-        tts.save(f"temp/{my_file_name}.mp3")
-        return my_file_name, trans_text
+    with col2:      
 
-    display_output_text = st.checkbox("Mostrar el texto traducido")
+        # get label names
+        label_names = model.names
+        # count categories
+        category_count = {}
+        for category in categories:
+            if category in category_count:
+                category_count[category] += 1
+            else:
+                category_count[category] = 1        
 
-    if st.button("Convertir"):
-        try:
-            result, output_text = text_to_speech(input_language, output_language, text)
-            audio_file = open(f"temp/{result}.mp3", "rb")
-            audio_bytes = audio_file.read()
-            st.markdown("## Tu audio:")
-            st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-            if display_output_text:
-                st.markdown("## Texto traducido:")
-                st.write(output_text)
-        except Exception as e:
-            st.error(f"Error al traducir o convertir el texto: {e}")
-
-    def remove_files(n):
-        mp3_files = glob.glob("temp/*.mp3")
-        if len(mp3_files) != 0:
-            now = time.time()
-            n_days = n * 86400
-            for f in mp3_files:
-                if os.stat(f).st_mtime < now - n_days:
-                    os.remove(f)
-                    print("Deleted ", f)
-
-    remove_files(7)
-
+        data = []        
+        # print category counts and labels
+        for category, count in category_count.items():
+            label = label_names[int(category)]            
+            data.append({"Categoría":label,"Cantidad":count})
+        data2 =pd.DataFrame(data)
+        
+        # agrupar los datos por la columna "categoria" y sumar las cantidades
+        df_sum = data2.groupby('Categoría')['Cantidad'].sum().reset_index() 
+        df_sum
 
            
 
